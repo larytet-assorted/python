dd888acd847831e93e2c397dec0467ccfb344707
arkady <larytet@yahoo.com>
2019-04-16 15:46:21 +0300

    Count columns

--- columns.py
+++ columns.py
@@ -5,10 +5,11 @@ import sys
 
 if __name__ == '__main__':
     columns = 0
-    datafile = sys.argv[1]
-    with open(datafile) as csv_file:
+    data_filename = sys.argv[1]
+    with open(data_filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter='\t')
         for row in csv_reader:
             if columns != len(row):
                 columns = len(row)
-                print("Columns {0}: {1}".format(columns, row[0])
+                print("Columns {0}, timestamp {1}".format(columns, row[0]))
+        print("Last line timestamp {0}".format(row[0]))
